69632a32b32a2ce5cfbea4dcb1885153a29e0fcd
kox <garsanzi@gmail.com>
2024-09-19 20:01:15 +0200

    specifying creating proposal campaign steps

--- app/createProposalCampaign.ts
+++ app/createProposalCampaign.ts
@@ -1,2 +1,18 @@
 console.log("Creating proposal campaign");
 
+
+// Upload Image campaign
+
+// Upload offchain campaign metadata
+
+// Create campaign onchain metadata
+
+// Create Metaplex Core Mint
+
+// Add plugins
+//  - attributes
+//  - Freeze delegate
+
+// Send to the client to sign it
+
+// ( Created )
